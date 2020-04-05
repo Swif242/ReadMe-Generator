51e794d9ffde0d80a4ca957783093687c5fa6b31
Jeremy N <no-reply>
2020-04-04 20:02:19 -0700

    got the api call to work and the index questions and asnswers to work

--- Develop/index.js
+++ Develop/index.js
@@ -1,57 +1,75 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-
+const axios = require("axios");
 
 
 
 const questions = [
-    {
-        name: "name",
-        type: "input",
-        message:"What is your Username?: "
-    },
+
     {
         name: "title",
         type: "input",
         message:"Project Title: "
     },
-    {
-        name: "description",
-        type: "input",
-        message:"Project Description: "
-    },
-    {
-        name: "contents",
-        type: "input",
-        message:"Table of Contents: "
-    },
-    {
-        name: "installation",
-        type: "input",
-        message:"Install Instructions: "
-    },
-    {
-        name: "usage",
-        type: "input",
-        message:"Usage: "
-    },
-    {
-        name: "license",
-        type: "input",
-        message:"License"
-    },
-    {
-        name: "contributing",
-        type: "input",
-        message:"Contributing"
-    },
-    {
-        name: "test",
-        type: "input",
-        message:"Tests: "
-    },
+    // {
+    //     name: "description",
+    //     type: "input",
+    //     message:"Project Description: "
+    // },
+    // {
+    //     name: "contents",
+    //     type: "input",
+    //     message:"Table of Contents: "
+    // },
+    // {
+    //     name: "installation",
+    //     type: "input",
+    //     message:"Install Instructions: "
+    // },
+    // {
+    //     name: "usage",
+    //     type: "input",
+    //     message:"Usage: "
+    // },
+    // {
+    //     name: "license",
+    //     type: "input",
+    //     message:"License: "
+    // },
+    // {
+    //     name: "contributing",
+    //     type: "input",
+    //     message:"Contributing: "
+    // },
+    // {
+    //     name: "test",
+    //     type: "input",
+    //     message:"Tests: "
+    // },
 
 ];
+
+
+
+inquirer.prompt(questions)
+.then(answers => {
+    // Use user feedback for... whatever!!
+    var data = JSON.stringify(answers)
+    fs.writeFile("responses.txt", data, (err) =>{
+        if (err) throw err;
+    console.log(answers);
+    console.log("the file has been saved");
+
+    });
+})
+.catch(error => {
+    if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+        console.log("dont work bro")
+    } else {
+        // Something else when wrong
+    }
+});
 
 function writeToFile(fileName, data) {
 }
--- Develop/utils/api.js
+++ Develop/utils/api.js
@@ -1,5 +1,28 @@
+const axios = require("axios");
+const inquirer = require("inquirer");
+
+
+
+
+
 const api = {
   getUser(username) {
+    inquirer
+      .prompt({
+        message: "Enter your GitHub username",
+        name: "username"
+      })
+      .then(function ({ username }) {
+        const queryUrl = `https://api.github.com/users/${username}`;
+        axios
+          .get(queryUrl)
+          .then(res => {
+            console.log(res.data);
+            const email = res.data.email
+            console.log(email);
+          });
+
+      });
 
   }
 };
